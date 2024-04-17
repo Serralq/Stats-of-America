a11d79da438c317c455a3b4fdaf40c6125c68abc
Alexander Dao <alexanderngocdao@gmail.com>
2024-04-17 09:40:02 -0400

    (backend) Sample csv and csv file reader

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-  fmt.Println("Hello World")
+	file, err := os.Open("Dataset/EPA_SmartLocationDatabase_V3_Jan_2021_Final.csv")
+
+	// Checks for the error
+	if err != nil {
+		log.Fatal("Error while reading the file", err)
+	}
+
+	// Closes the file
+	defer file.Close()
+
+	// The csv.NewReader() function is called in
+	// which the object os.File passed as its parameter
+	// and this creates a new csv.Reader that reads
+	// from the file
+	reader := csv.NewReader(file)
+
+	// ReadAll reads all the records from the CSV file
+	// and Returns them as slice of slices of string
+	// and an error if any
+	records, err := reader.ReadAll()
+
+	// Checks for the error
+	if err != nil {
+		fmt.Println("Error reading records")
+	}
+
+	// Loop to iterate through
+	// and print each of the string slice
+	for _, eachrecord := range records {
+		fmt.Println(eachrecord)
+	}
 }
