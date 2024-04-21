3a11614b71ec1370b13dc98c10090cb09257c547
Sniped <colin@colinmcc.me>
2024-04-20 20:12:22 -0400

    (frontend) split types

--- frontend/src/app/types/similarityDataSet.ts
+++ frontend/src/app/types/similarityDataSet.ts
@@ -1,8 +1,10 @@
 export interface SimilarityDataSet {
-	[key: string]: {
-		state: string;
-		comparedData: [[string, string], [string, string]];
-		path: [string, string];
-		similarity: number;
-	}[];
+	[key: string]: ComparisonElement[];
+}
+
+export interface ComparisonElement {
+	state: string;
+	comparedData: [[string, string], [string, string]];
+	path: [string, string];
+	similarity: number;
 }
